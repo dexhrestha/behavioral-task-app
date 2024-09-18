566179a9c1ede8c656157b97839f021aed570ac7
dexhrestha <dipesh.shrestha009@gmail.com>
2024-09-18 23:58:21 +0100

    Updated graphs

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ st.download_button(
 )
 
 
-
 go_df = state[state['stateChange'] == 'GO'].drop_duplicates('trialNumber',keep='first')
 moving_df = state[state['stateChange'] == 'MOVING'].drop_duplicates('trialNumber',keep='first')
 stop_df = state[state['stateChange'] == 'STOP'].drop_duplicates('trialNumber',keep='first')
@@ -68,12 +67,24 @@ trial_sample['stepSize'] = pd.Categorical(trial_sample['stepSize'])
 
 trial_sample['producedTime'] = first_attempt_duration_df['duration']
 
-trial_sample['producedTime'] = (trial_sample['interlmdistance']/trial_sample['interlmdistance'].apply(abs)*trial_sample['producedTime'])/1000
-trial_sample['trueTime'] = (trial_sample['interlmdistance']/trial_sample['interlmdistance'].apply(abs)*trial_sample['trueTime'])/1000
-trial_sample['reactionTime'] = reaction_duration_df['duration']/1000
-trial_sample.loc[:,'jitter_producedTime'] =  trial_sample['producedTime'].apply(lambda x:x + random.uniform(-0.25, 0.25))
-trial_sample.loc[:,'jitter_reactionTime'] = trial_sample['reactionTime'].apply(lambda x:x + random.uniform(-0.25, 0.25))
-trial_sample.loc[:,'jitter_trueTime'] = trial_sample['trueTime'].apply(lambda x:x + random.uniform(-0.25, 0.25))
+trial_sample['producedTime_s'] = (trial_sample['producedTime']/1000).round(3)
+trial_sample['trueTime_s'] = (trial_sample['trueTime']/1000).round(3)
+
+#producedtime_with_direction
+trial_sample['producedTime_dir'] = (trial_sample['interlmdistance']/trial_sample['interlmdistance'].apply(abs)*trial_sample['producedTime_s'])
+#truetime_with_direction
+trial_sample['trueTime_dir'] = (trial_sample['interlmdistance']/trial_sample['interlmdistance'].apply(abs)*trial_sample['trueTime_s'])
+
+# add jitter
+trial_sample.loc[:,'jitter_producedTime_dir'] =  trial_sample['producedTime_dir'].apply(lambda x:x + random.uniform(-0.05, 0.05))
+trial_sample.loc[:,'jitter_trueTime_dir'] = trial_sample['trueTime_dir'].apply(lambda x:x + random.uniform(-0.05, 0.05))
+
+# calcualte reaction time with jitter
+trial_sample['reactionTime_s'] = (reaction_duration_df['duration']/1000).round(3)
+trial_sample.loc[:,'jitter_reactionTime_s'] = trial_sample['reactionTime_s'].apply(lambda x:x + random.uniform(-0.05, 0.05))
+
+speed_1 = trial_sample[trial_sample['stepSize']==20]
+speed_2 = trial_sample[trial_sample['stepSize']==15]
 
 
 trial_type = st.selectbox("Trial Type",['All','Mental','Visual'])
@@ -84,13 +95,41 @@ if trial_type!='All':
     elif trial_type=="Visual":
         trial_sample = trial_sample[trial_sample['isTrain']]
 
-fig ,ax = plt.subplots()
 
-speed_1 = trial_sample[trial_sample['stepSize']==20]
-speed_2 = trial_sample[trial_sample['stepSize']==15]
+speed_1_err_prod = speed_1.groupby('trueTime_dir').agg(mean_produced_time=('producedTime_s', 'mean'),
+                                                  std_produced_time=('producedTime_s', 'std')).reset_index()
 
-ax = speed_1.plot(x='jitter_trueTime',y='producedTime',kind='scatter',c='red',ax=ax,label='1x',alpha=0.7)
-ax = speed_2.plot(x='jitter_trueTime',y='producedTime',kind='scatter',c='blue',ax=ax,label='0.75x',alpha=0.7)
+speed_1_err_reac = speed_1[speed_1['reactionTime_s']<5].groupby('trueTime_dir').agg(mean_reaction_time=('reactionTime_s', 'mean'),
+                                                  std_reaction_time=('reactionTime_s', 'std')).reset_index()
+                                         
+
+speed_1_err_prod['dir'] = speed_1_err_prod['trueTime_dir'] / speed_1_err_prod['trueTime_dir'].abs()
+speed_1_err_prod['mean_produced_time'] = speed_1_err_prod['mean_produced_time'] * speed_1_err_prod['dir']
+
+
+speed_2_err_prod = speed_2.groupby('trueTime_dir').agg(mean_produced_time=('producedTime_s', 'mean'),
+                                                  std_produced_time=('producedTime_s', 'std')).reset_index()
+
+speed_2_err_reac = speed_2[speed_2['reactionTime_s']<5].groupby('trueTime_dir').agg(mean_reaction_time=('reactionTime_s', 'mean'),
+                                                  std_reaction_time=('reactionTime_s', 'std')).reset_index()
+                                         
+
+speed_2_err_prod['dir'] = speed_2_err_prod['trueTime_dir'] / speed_2_err_prod['trueTime_dir'].abs()
+speed_2_err_prod['mean_produced_time'] = speed_2_err_prod['mean_produced_time'] * speed_2_err_prod['dir']
+
+
+fig, ax = plt.subplots()
+
+
+ax = speed_1.plot(x='jitter_trueTime_dir',y='producedTime_dir',kind='scatter',c='red',ax=ax,label='1x',alpha=0.5,s=5)
+ax.errorbar(x=speed_1_err_prod['trueTime_dir'],y=speed_1_err_prod['mean_produced_time'],c='red',fmt='o',alpha=0.5,yerr=speed_1_err_prod['std_produced_time'])
+
+ax = speed_2.plot(x='jitter_trueTime_dir',y='producedTime_dir',kind='scatter',c='blue',ax=ax,label='0.75x',alpha=0.5,s=5)
+ax.errorbar(x=speed_2_err_prod['trueTime_dir'],y=speed_2_err_prod['mean_produced_time'],c='blue',fmt='o',alpha=0.5,yerr=speed_2_err_prod['std_produced_time'])
+
+plt.plot([-6,6],[-6,6],c='grey',linewidth=0.5,linestyle=(5,(10,3)))
+
+
 ax.tick_params(axis='x', rotation=45)
 ax.tick_params(axis='y', rotation=45)
 ax.legend(loc='upper left')
@@ -100,17 +139,18 @@ st.subheader('Comparison of True and Produced Time')
 st.pyplot(fig)
 
 fig ,ax = plt.subplots()
-ax = speed_1.plot(y='reactionTime',x='jitter_trueTime',kind='scatter',c='red',ax=ax,label='1x',alpha=0.7)
-ax = speed_2.plot(y='reactionTime',x='jitter_trueTime',kind='scatter',c='blue',ax=ax,label='0.75x',alpha=0.7)
+ax = speed_1.plot(y='reactionTime_s',x='jitter_trueTime_dir',kind='scatter',c='red',ax=ax,label='1x',alpha=0.7,s=5)
+ax.errorbar(x=speed_1_err_reac['trueTime_dir'],y=speed_1_err_reac['mean_reaction_time'],c='red',fmt='o',alpha=0.5)
 
+ax = speed_2[speed_2['reactionTime_s']<=5].plot(y='reactionTime_s',x='jitter_trueTime_dir',kind='scatter',c='blue',ax=ax,label='0.75x',alpha=0.7,s=5)
+ax.errorbar(x=speed_2_err_reac['trueTime_dir'],y=speed_2_err_reac['mean_reaction_time'],c='blue',fmt='o',alpha=0.5)
+
+ax.set_ylim([0,5])
 ax.tick_params(axis='x', rotation=45)
 ax.tick_params(axis='y', rotation=45)
 ax.legend(loc='upper left')
 ax.set_xlabel("True Time (s)")
 ax.set_ylabel("Reaction Time (s)")
-ax.plot([-6,6],[speed_1['reactionTime'].mean(),speed_1['reactionTime'].mean()],linestyle='dashed',c='red')
-ax.plot([-6,6],[speed_2['reactionTime'].mean(),speed_2['reactionTime'].mean()],linestyle='dashed',c='blue')
-ax.set_ylim([0,5])
 st.subheader('Reaction Time vs True Time')
 st.pyplot(fig)
 
